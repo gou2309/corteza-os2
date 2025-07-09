450105e27e571d61738201d7ceeae49bc45ad80c
gou2309 <laputaserpiente2093@gmail.com>
2025-07-08 21:54:57 -0600

    Agrega ruta /postinstall y actualiza rutas del servidor

--- server/index.mjs
+++ server/index.mjs
@@ -12,14 +12,19 @@ const __dirname = path.dirname(__filename);
 // Servir archivos estáticos desde /public
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Ruta raíz que sirve welcome.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/welcome.html'));
+});
+
 // Ruta para el dashboard
 app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/dashboard.html'));
 });
 
-// Ruta raíz que sirve welcome.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/welcome.html'));
+// Ruta post-instalación
+app.get('/postinstall', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/postinstall.html'));
 });
 
 // Iniciar el servidor
